feaf3698b1552ed1f3c66d13569f1fa7ea7d48e0
tv0924@icloud.com <j93es@naver.com>
2024-09-26 11:59:04 +0900

    [Update] app.js

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -27,7 +27,7 @@ app.use(
   })
 );
 
-app.use(express.json());
+app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: false }));
 
 app.use(rateLimiter.makeLimit(60, 200));
