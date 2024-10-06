264a5cd396cf490cba3371e0e69c6119b536871a
tv0924@icloud.com <j93es@naver.com>
2024-10-06 18:31:55 +0900

    [Style] posting bottom margin

--- frontend/src/pages/body/Posting.tsx
+++ frontend/src/pages/body/Posting.tsx
@@ -19,7 +19,7 @@ function Posting({ markdownContent }: { markdownContent: string }) {
         width: "90%",
         display: "flex",
         justifyContent: "center",
-        marginBottom: "4.5rem",
+        marginBottom: "4rem",
       }}
     >
       <div style={{ width: "100%" }}>
