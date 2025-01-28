140db92a89b495b807d56656ae4b0ef321771cc6
tv0924@icloud.com <j93es@naver.com>
2025-01-29 01:51:04 +0900

    [Update] backend errorHandler encodeURI

--- backend/src/utils/errorHandler.ts
+++ backend/src/utils/errorHandler.ts
@@ -28,7 +28,7 @@ export class ErrorHandler {
   notFound = (error: any, req: any, res: any, next: any) => {
     if (error instanceof NotFoundError) {
       const code = 404;
-      const message = encodeURIComponent("요청하신 정보를 찾을 수 없습니다.");
+      const message = "요청하신 정보를 찾을 수 없습니다.";
 
       customLogger.log("NotFoundError", error.message, req);
       this.redirectErrorPage(res, code, message);
@@ -41,7 +41,7 @@ export class ErrorHandler {
   badRequestError = (error: any, req: any, res: any, next: any) => {
     if (error instanceof BadRequestError) {
       const code = 400;
-      const message = encodeURIComponent("잘못된 요청입니다.");
+      const message = "잘못된 요청입니다.";
 
       customLogger.log("BadRequestError", error.message, req);
       this.redirectErrorPage(res, code, message);
@@ -53,10 +53,7 @@ export class ErrorHandler {
   tooManyRequestsError = (error: any, req: any, res: any, next: any) => {
     if (error instanceof TooManyRequestsError) {
       const code = 429;
-      const message = encodeURIComponent(
-        "접속량이 많습니다. 잠시 후 다시 시도해주세요."
-      );
-
+      const message = "접속량이 많습니다. 잠시 후 다시 시도해주세요.";
       customLogger.warn("TooManyRequestsError", error.message, req);
       this.redirectErrorPage(res, code, message);
       return;
@@ -68,7 +65,7 @@ export class ErrorHandler {
   forbiddenError = (error: any, req: any, res: any, next: any) => {
     if (error instanceof ForbiddenError) {
       const code = 403;
-      const message = encodeURIComponent("금지된 접근입니다.");
+      const message = "금지된 접근입니다.";
 
       customLogger.warn("ForbbidenError", error.message, req);
       this.redirectErrorPage(res, code, message);
@@ -84,7 +81,7 @@ export class ErrorHandler {
       res.locals.error = error;
 
       const code = 500;
-      const message = encodeURIComponent("예기치 못한 문제가 발생하였습니다.");
+      const message = "예기치 못한 문제가 발생하였습니다.";
 
       customLogger.error("InternalServerError", error.message, req);
       this.redirectErrorPage(res, code, message);
