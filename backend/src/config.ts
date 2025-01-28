0fea5a57cddfe81beafdcea26e121dd835b3d53a
tv0924@icloud.com <j93es@naver.com>
2025-01-29 01:47:53 +0900

    [Update] config default url

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -3,7 +3,7 @@ import path from "path";
 export const PORT = process.env.PORT || 8081;
 
 export const errorPageUrl =
-  process.env.ERROR_PAGE_URL || "https://j93.es/api/error-page/";
+  process.env.ERROR_PAGE_URL || "https://j93.es/api/error-page";
 
 export const whitelist = JSON.parse(
   process.env.CORS_WHITE_LIST || JSON.stringify(["http://localhost:3000"])
