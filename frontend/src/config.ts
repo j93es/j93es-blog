7c684f7831eca7724427ce73235aee9ed9d428ec
tv0924@icloud.com <j93es@naver.com>
2025-02-06 01:41:54 +0900

    [Update] meta description

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,4 +2,4 @@ export const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:8080/";
 export const defaultTitle = process.env.REACT_APP_DEFAULT_TITLE || "j93es blog";
 export const defaultDescription =
   process.env.REACT_APP_DEFAULT_DESCRIPTION ||
-  "j93es blog는 개발과 관련한 여러 경험을 공유하고 기술적인 해결 과정을 기록합니다. 때로는 카메라를 들고 세상을 담아내기도 합니다. 개발과 사진이 함께 어우러지는 공간에서, 작은 발견과 영감을 나눌 수 있기를 바랍니다.";
+  "개발 경험과 기술적인 해결 과정을 기록하며, 때로는 사진으로 세상을 담아냅니다. j93es blog에서 작은 발견과 영감을 나눌 수 있기를 바랍니다.";
