13aac46219e8d5751955da002d5a0aa69058d08a
tv0924@icloud.com <j93es@naver.com>
2025-02-25 03:29:30 +0900

    [Update] device detect logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,15 +5,14 @@
 // Local
 import useDesktopSafariBackFix from "customHooks/useDesktopSafariBackFix";
 import useScrollToTop from "customHooks/useScrollToTop";
-import { isMacOs, isSafari } from "mobile-device-detect";
+import { isMacOs, isSafari, isIOS, isMobileSafari } from "mobile-device-detect";
 import Header from "pages/header/Header";
 import Body from "pages/body/Body";
 import Footer from "pages/footer/Footer";
 import "App.css";
 
 const App = () => {
-  console.log("isMacOs", isMacOs);
-  useDesktopSafariBackFix(isMacOs && isSafari);
+  useDesktopSafariBackFix(isMacOs && isSafari && !isIOS && !isMobileSafari);
   useScrollToTop();
 
   return (
