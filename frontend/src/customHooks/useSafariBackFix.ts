55515794cbe424e77a6ac66121e1b55d0e5d7cb5
tv0924@icloud.com <j93es@naver.com>
2025-02-25 02:05:25 +0900

    [Test]

--- frontend/src/customHooks/useSafariBackFix.ts
+++ frontend/src/customHooks/useSafariBackFix.ts
@@ -11,12 +11,6 @@ const useSafariBackFix = () => {
   useEffect(() => {
     if (!isSafari()) return;
 
-    const handlePageShow = (event: { persisted: boolean }) => {
-      if (event.persisted) {
-        window.location.reload();
-      }
-    };
-
     // popstate 이벤트가 발생하면 페이지를 새로고침
     // setTimeout을 사용하면 popstate가 트리거된 후 약간의 시간이 지나도록 강제하여, 브라우저의 상태 변화가 완료될 가능성을 높혀 안정성 증대(정확하진 않음)
     const handlePopState = () => {
@@ -25,10 +19,8 @@ const useSafariBackFix = () => {
       }, 10);
     };
 
-    window.addEventListener("pageshow", handlePageShow);
     window.addEventListener("popstate", handlePopState);
     return () => {
-      window.removeEventListener("pageshow", handlePageShow);
       window.removeEventListener("popstate", handlePopState);
     };
   }, []);
