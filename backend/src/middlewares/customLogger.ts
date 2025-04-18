5d207253837c17ba0fa34507075f79cf590ad75e
tv0924@icloud.com <j93es@naver.com>
2025-04-18 11:37:14 +0900

    [Docs] information-protection-policy

--- backend/src/middlewares/customLogger.ts
+++ backend/src/middlewares/customLogger.ts
@@ -2,31 +2,8 @@ import { Request, Response, NextFunction } from "express";
 import { requestWriter } from "./index";
 
 export class CustomLogger {
-  private formatDateToCustomString = (date: Date): string => {
-    const koreaDate = new Date(date.getTime() + 9 * 60 * 60 * 1000);
-    const day = koreaDate.getUTCDate().toString().padStart(2, "0");
-    const monthNames = [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-    ];
-    const month = monthNames[koreaDate.getUTCMonth()];
-    const year = koreaDate.getUTCFullYear();
-    const hours = koreaDate.getUTCHours().toString().padStart(2, "0");
-    const minutes = koreaDate.getUTCMinutes().toString().padStart(2, "0");
-    const seconds = koreaDate.getUTCSeconds().toString().padStart(2, "0");
-
-    const formattedDate = `${day}/${month}/${year}:${hours}:${minutes}:${seconds} +0900`;
-    return formattedDate;
+  private getTimeMsg = (date: Date): string => {
+    return new Date(date.getTime() + 9 * 60 * 60 * 1000).toISOString();
   };
 
   private makeSimpleMsg = (head: string, message: string) => {
@@ -35,13 +12,14 @@ export class CustomLogger {
 
   private makeMsg = (head: string, message: string, req?: Request) => {
     if (!req)
-      return `[${this.formatDateToCustomString(
-        new Date()
-      )}] ${this.makeSimpleMsg(head, message)}`;
+      return `[${this.getTimeMsg(new Date())}] ${this.makeSimpleMsg(
+        head,
+        message
+      )}`;
 
     return `${requestWriter.getId(req)} ${requestWriter.getIp(
       req
-    )} - - [${this.formatDateToCustomString(new Date())}] ${this.makeSimpleMsg(
+    )} - - [${this.getTimeMsg(new Date())}] ${this.makeSimpleMsg(
       head,
       message
     )} - ${req.method} ${req.originalUrl} - ${req.headers["user-agent"]}`;
--- backend/src/middlewares/requestWriter.ts
+++ backend/src/middlewares/requestWriter.ts
@@ -17,62 +17,62 @@ export class RequestWriter {
     return `${req.headers.removedIp}`;
   };
 
-  addIp = (req: Request, res: Response, next: NextFunction) => {
-    const clientIp = this.getClientIp(req);
-    const removedIp = this.removeIpPart(clientIp);
+  // addIp = (req: Request, res: Response, next: NextFunction) => {
+  //   const clientIp = this.getClientIp(req);
+  //   const removedIp = this.removeIpPart(clientIp);
 
-    req.headers.removedIp = removedIp;
-    next();
-  };
+  //   req.headers.removedIp = removedIp;
+  //   next();
+  // };
 
-  private getClientIp = (req: Request): string => {
-    const xForwardedFor = req.headers["x-forwarded-for"];
-    if (typeof xForwardedFor === "string") {
-      return xForwardedFor.split(",")[0].trim();
-    } else if (Array.isArray(xForwardedFor) && xForwardedFor.length > 0) {
-      return xForwardedFor[0].trim();
-    }
+  // private getClientIp = (req: Request): string => {
+  //   const xForwardedFor = req.headers["x-forwarded-for"];
+  //   if (typeof xForwardedFor === "string") {
+  //     return xForwardedFor.split(",")[0].trim();
+  //   } else if (Array.isArray(xForwardedFor) && xForwardedFor.length > 0) {
+  //     return xForwardedFor[0].trim();
+  //   }
 
-    const xRealIp = req.headers["x-real-ip"];
-    if (typeof xRealIp === "string") {
-      return xRealIp;
-    } else if (Array.isArray(xRealIp) && xRealIp.length > 0) {
-      return xRealIp[0].trim();
-    }
+  //   const xRealIp = req.headers["x-real-ip"];
+  //   if (typeof xRealIp === "string") {
+  //     return xRealIp;
+  //   } else if (Array.isArray(xRealIp) && xRealIp.length > 0) {
+  //     return xRealIp[0].trim();
+  //   }
 
-    return req.socket.remoteAddress || "";
-  };
+  //   return req.socket.remoteAddress || "";
+  // };
 
-  private removeIpPart = (ipStr: string): string => {
-    // IPv4 처리
-    if (ipaddr.IPv4.isValid(ipStr)) {
-      try {
-        const addr = ipaddr.IPv4.parse(ipStr);
-        const fullIp: string = addr.toNormalizedString();
-        const parts: string[] = fullIp.split(".");
-        const newParts = parts.map((elem, idx) =>
-          idx < ipV4RemainIndex ? elem : ""
-        );
-        return newParts.join(".");
-      } catch (e) {
-        return "";
-      }
-    }
-    // IPv6 처리
-    else if (ipaddr.IPv6.isValid(ipStr)) {
-      try {
-        const addr = ipaddr.IPv6.parse(ipStr);
-        const fullIp: string = addr.toNormalizedString();
-        const parts: string[] = fullIp.split(":");
-        const newParts = parts.map((elem, idx) =>
-          idx < ipV6RemainIndex ? elem : ""
-        );
-        return newParts.join(":");
-      } catch (e) {
-        return "";
-      }
-    } else {
-      return "";
-    }
-  };
+  // private removeIpPart = (ipStr: string): string => {
+  //   // IPv4 처리
+  //   if (ipaddr.IPv4.isValid(ipStr)) {
+  //     try {
+  //       const addr = ipaddr.IPv4.parse(ipStr);
+  //       const fullIp: string = addr.toNormalizedString();
+  //       const parts: string[] = fullIp.split(".");
+  //       const newParts = parts.map((elem, idx) =>
+  //         idx < ipV4RemainIndex ? elem : ""
+  //       );
+  //       return newParts.join(".");
+  //     } catch (e) {
+  //       return "";
+  //     }
+  //   }
+  //   // IPv6 처리
+  //   else if (ipaddr.IPv6.isValid(ipStr)) {
+  //     try {
+  //       const addr = ipaddr.IPv6.parse(ipStr);
+  //       const fullIp: string = addr.toNormalizedString();
+  //       const parts: string[] = fullIp.split(":");
+  //       const newParts = parts.map((elem, idx) =>
+  //         idx < ipV6RemainIndex ? elem : ""
+  //       );
+  //       return newParts.join(":");
+  //     } catch (e) {
+  //       return "";
+  //     }
+  //   } else {
+  //     return "";
+  //   }
+  // };
 }
