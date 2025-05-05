268d02019ab4e0d5c76c37c01f70ae1dd2b99d5c
tv0924@icloud.com <j93es@naver.com>
2025-05-05 21:39:37 +0900

    [Update] health check domain structure

--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 
+import { ForbiddenError } from "../models/error";
 import { postingIndexServ } from "../service/index";
 import { apiDir, monitoringKey } from "../config";
 import { eachErrorHandler } from "../middlewares/index";
@@ -10,12 +11,13 @@ router.get("/posting-index.json", (req: Request, res: Response) => {
   res.json(postingIndexServ.get());
 });
 
-router.get(
-  `/monitoring/health/${monitoringKey}`,
-  (req: Request, res: Response) => {
-    res.status(200).send("OK");
+router.get(`/monitoring/health`, (req: Request, res: Response) => {
+  if (req.query.key !== monitoringKey) {
+    throw new ForbiddenError("Invalid monitoring key");
   }
-);
+  res.setHeader("Cache-Control", "no-cache, no-store");
+  res.status(200).send("OK");
+});
 
 router.use(express.static(apiDir));
 
